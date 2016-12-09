10022ac61193b17782641963686f6e58926f3638
neiderm <ded.reckning@gmail.com>
2016-12-09 14:17:24 -0600

    Tweaking

--- code/uvmesh.py
+++ code/uvmesh.py
@@ -2,13 +2,20 @@
 #
 # UV sphere mesh experiment
 # reference:
-#   https://gamedevdaily.io/four-ways-to-create-a-mesh-for-a-sphere-d7956b825db4#.jgbu4qs54
-#
 #   https://github.com/zhongyn/opengl-8-ball-pool-game
+#
+# also:
+#   https://gamedevdaily.io/four-ways-to-create-a-mesh-for-a-sphere-d7956b825db4#.jgbu4qs54
+# 
+#   for j in parallels_count:
+#    parallel = PI * (j+1) / parallels_count  # GN: +1 offset is arbitrary?
+#     for i in meridians_count:
+#       meridian = 2.0 * PI * i / meridians_count
+#       return spherical_to_cartesian(meridian, parallel)
 #
 import math
 
-PI = 3.14159
+PI = math.pi  # 3.14159
 
 def main():
 #  radius = int(input("circle radius? "))
@@ -20,10 +27,12 @@ def main():
 
   for i in range(parallels_count):
 
-    #thetaR = PI * (i+1) / parallels_count    # GN: why +1 ?
-    thetaR = (i * PI / parallels_count) #  - (PI/2)
+    thetaR =      0  + (i * PI / parallels_count)  #  Z
+    thetaR =  -PI/2  + (i * PI / parallels_count)  # G
 
-    print "theta = %fd %.3fr  (R=%.3f)" % (thetaR * 180.0/PI, thetaR, math.cos(thetaR))
+    rlat = math.cos(thetaR)    # circle radius at this parallel
+
+    print "theta = %fd (%.3fr)  [R=%.3f]" % (thetaR * 180.0/PI, thetaR, rlat)
 
     for j in range(meridians_count):
 
@@ -31,14 +40,16 @@ def main():
 
       vec3 = g_spherical_to_cartesian(phi, thetaR)
 
-      print "  v(%d) [%.4f] [%0.3f, %0.3f, %0.3f]" % (i*parallels_count+j, phi, 
+      print "  v(%2d) [%7.4f] [%7.3f, %7.3f, %7.3f]" % (i*parallels_count+j, phi, 
           vec3[0], vec3[1], vec3[2])
+
 
 # zhongyaonan method
 def z_spherical_to_cartesian(phi, thetaR):
 
-  rlat = math.sin(thetaR)  # circle radius at this parallel
-  x = rlat * math.sin(phi)      
+  # glm::vec3 v1 = glm::vec3(sin(theta) * sin(phi), cos(theta), sin(theta) * cos(phi));
+  # glm::vec3 v2 = glm::vec3(sin(theta + stepsk) * sin(phi), cos(theta + stepsk), sin(theta + stepsk) * cos(phi));
+  x = math.sin(theta) * math.sin(phi)      
   y = math.cos(thetaR)
   z = rlat * math.cos(phi)
   vec3 = [x, y, z]
@@ -47,11 +58,13 @@ def z_spherical_to_cartesian(phi, thetaR):
 
 # my method
 def g_spherical_to_cartesian(phi, thetaR):
-  thetaR -=  PI / 2 
-  rlat = math.cos(thetaR)  # circle radius at this parallel
-  x = rlat * math.cos(phi)      
+
+#  thetaR -=  PI / 2 
+
+  r = math.cos(thetaR)  # circle radius at this parallel
+  x = r * math.cos(phi)      
   y = math.sin(thetaR)
-  z = rlat * math.sin(phi)
+  z = r * math.sin(phi)
   vec3 = [x, y, z]
 
   return vec3
